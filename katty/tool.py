e3f687efc7d48915bb32a83be84a77c9da592758
Soufyan Abdellati <abdellatis149@gmail.com>
2024-06-11 13:35:09 +0000

    Added a warning when no variabels are set

--- katty/tool.py
+++ katty/tool.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from communicators.get_findings import get_findings
 from communicators.get_random_ooi import get_random_ooi
@@ -11,7 +12,11 @@ logger = logging.getLogger("katty")
 ORGANISATION_ID = ""
 CHOSEN_PLUGIN = ""
 
+
 if __name__ == "__main__":
+    if not ORGANISATION_ID or not CHOSEN_PLUGIN:
+        logging.fatal("Not all variables are set!")
+        sys.exit(1)
     while True:
         choice = input(
             "What do you want to do?\n1).\tGet the findings.\n2).\tToggle a plugin.\n3).\tGet a random OOI.\n"
